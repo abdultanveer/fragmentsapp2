0cd11f5b350f3bee0402141c4487aed26dd715c0
abdul <abdul.tanveer@gmail.com>
2019-07-16 08:56:29 +0530

    fragment communication -- phone

--- app/src/main/java/com/example/fragmentsapp/DetailsFragment.java
+++ app/src/main/java/com/example/fragmentsapp/DetailsFragment.java
@@ -2,6 +2,8 @@ package com.example.fragmentsapp;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +16,13 @@ import android.widget.TextView;
  */
 public class DetailsFragment extends Fragment {
 
-TextView detailsTextView;
+    public static final String MY_KEY = "somekey" ;
+    TextView detailsTextView;
     public DetailsFragment() {
         // Required empty public constructor
     }
+
+
 
 
     @Override
@@ -27,9 +32,15 @@ TextView detailsTextView;
         //FragmentDetailsScreen fragmentDetailsScreen = new FragmentDetailsScreen(getContext());
         View view = inflater.inflate(R.layout.fragment_details, container, false);
         detailsTextView = view.findViewById(R.id.newsDetailsTextview);
+
+        String data = getArguments().getString(MY_KEY);
+        detailsTextView.setText(data);
+
+
         return view;
 
     }
+
 
     public void updateTextView(String headline) {//c
         detailsTextView.setText(headline);
--- app/src/main/java/com/example/fragmentsapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentsapp/MainActivity.java
@@ -11,12 +11,12 @@ public class MainActivity extends AppCompatActivity implements HeadlinesFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//1
-        SimpleFragment simpleFragment = SimpleFragment.newInstance();
 
+       /* SimpleFragment simpleFragment = SimpleFragment.newInstance();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction =  fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment_container,simpleFragment);
-        fragmentTransaction.commit();
+        fragmentTransaction.commit();*/
     }
 
 
@@ -24,6 +24,26 @@ public class MainActivity extends AppCompatActivity implements HeadlinesFragment
     @Override
     public void onArticleSelected(String headline) {//b
         DetailsFragment detailsFragment = (DetailsFragment) getSupportFragmentManager().findFragmentById(R.id.detailsFragment);
-        detailsFragment.updateTextView(headline);
+        if (detailsFragment != null) {//it means my app has been installed on a tablet
+            detailsFragment.updateTextView(headline);
+        }
+        else{ // it means that my app is installed on the phone
+
+            DetailsFragment newFragment = new DetailsFragment();
+            Bundle args = new Bundle();
+            args.putString(DetailsFragment.MY_KEY, headline);
+            newFragment.setArguments(args);
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.my_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+
     }
 }
