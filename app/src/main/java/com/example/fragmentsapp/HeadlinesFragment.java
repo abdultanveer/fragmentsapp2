78171b2566d6ddec8416c9cf664a2d513e6f88eb
abdul <abdul.tanveer@gmail.com>
2019-07-16 06:56:01 +0530

    listview item click

--- app/src/main/java/com/example/fragmentsapp/HeadlinesFragment.java
+++ app/src/main/java/com/example/fragmentsapp/HeadlinesFragment.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HeadlinesFragment extends Fragment {
-
+public class HeadlinesFragment extends Fragment implements AdapterView.OnItemClickListener {
+    ListView headlinesListView;
 
     public HeadlinesFragment() {
         // Required empty public constructor
@@ -23,7 +26,17 @@ public class HeadlinesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false);//3
+        View view = inflater.inflate(R.layout.fragment_headlines, container, false);//3
+        headlinesListView = view.findViewById(R.id.headlineslistview);
+
+
+        headlinesListView.setOnItemClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getContext(), item, Toast.LENGTH_SHORT).show();
+    }
 }
